e3c9c38d466e3600f5403476badecef3dd2a3e63
msmoiz <mustafa.moiz125@gmail.com>
2021-04-19 10:22:43 -0700

    Set Reinforcements stats.
    Per specs on Kingdom Rush fandom page.

--- Source/RushReborn/Public/Abilities/ReinforcementCharacter.cpp
+++ Source/RushReborn/Public/Abilities/ReinforcementCharacter.cpp
@@ -5,6 +5,11 @@
 AReinforcementCharacter::AReinforcementCharacter()
 {
 	Stats = CreateDefaultSubobject<UStatsComponent>(TEXT("Stats"));
+	Stats->MaxHealth = Stats->CurrentHealth = 30;
+	Stats->Armor = 0;
+	Stats->MagicResistance = 0;
+	Stats->AttackDamage = FFloatRange(1, 2);
+	Stats->AttackRate = 1.f;
 }
 
 uint8 AReinforcementCharacter::GetTeamId()
