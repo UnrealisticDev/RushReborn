4737cf8853149bd114baabb0779edadd225301bf
msmoiz <mustafa.moiz125@gmail.com>
2021-04-19 13:34:47 -0700

    Create Combatant interface.
    To be implemented by all actors that can engage other actors.

---
+++ Source/RushReborn/Public/Combat/CombatantInterface.h
@@ -0,0 +1,21 @@
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "CombatantInterface.generated.h"
+
+UINTERFACE(meta = (CannotImplementInterfaceInBlueprints))
+class UCombatantInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ICombatantInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual bool IsEngaged() const = 0;
+	virtual void SetEngaged(bool bNewEngaged) = 0;
+	virtual bool IsAlive() const = 0;
+};
