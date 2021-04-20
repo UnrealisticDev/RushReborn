de5f8f942b904c1f4c35db1b66952198b8d90384
msmoiz <mustafa.moiz125@gmail.com>
2021-04-20 02:07:01 -0700

    Create and use SpawnFollowInterface.
    - Declare SpawnFollowInterface, to be used by spawn gate and AI tasks to move invaders along path to base
    - Spawn gate sets chosen path if spawned actor is a spline follower

--- Source/RushReborn/Public/Invaders/SpawnGate.cpp
+++ Source/RushReborn/Public/Invaders/SpawnGate.cpp
@@ -2,6 +2,7 @@
 #include "SplineRepeater.h"
 #include "TsunamiSpawnComponents.h"
 #include "Components/SplineComponent.h"
+#include "Invaders/SplineFollowInterface.h"
 
 ASpawnGate::ASpawnGate()
 {
@@ -17,8 +18,8 @@ void ASpawnGate::Spawn(FTsunamiSpawnContext Context)
 	USplineComponent* ChosenPath = PossiblePaths[FMath::Rand() % PossiblePaths.Num()];
 
 	AActor* SpawnedActor = GetWorld()->SpawnActorAbsolute(Context.SpawnClass, ChosenPath->GetTransformAtSplinePoint(0, ESplineCoordinateSpace::World));
-	if (SpawnedActor)
+	if (ISplineFollowInterface* SpawnedSplineFollower = Cast<ISplineFollowInterface>(SpawnedActor))
 	{
-		// @todo: set path on actor
+		SpawnedSplineFollower->SetSplineToFollow(ChosenPath);
 	}
 }
---
+++ Source/RushReborn/Public/Invaders/SplineFollowInterface.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "SplineFollowInterface.generated.h"
+
+class USplineComponent;
+
+UINTERFACE()
+class USplineFollowInterface: public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ISplineFollowInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void SetSplineToFollow(USplineComponent* Spline) {}
+	virtual void StartSplineMovement() {}
+	virtual void StopSplineMovement() {}
+};
