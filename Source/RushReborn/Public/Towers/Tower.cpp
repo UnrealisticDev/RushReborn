0358be0662873a496059b1022a204efaf030733b
msmoiz <mustafa.moiz125@gmail.com>
2021-04-21 15:22:12 -0700

    Add team agent interface to towers.
    Naturally, all are on the player team.

---
+++ Source/RushReborn/Public/Towers/Tower.cpp
@@ -0,0 +1,8 @@
+#include "Tower.h"
+#include "Combat/Teams.h"
+
+uint8 ATower::GetTeamId()
+{
+	return ETeams::Player;
+}
+
--- Source/RushReborn/Public/Towers/Tower.h
+++ Source/RushReborn/Public/Towers/Tower.h
@@ -1,17 +1,20 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Combat/TeamInterface.h"
 #include "GameFramework/Pawn.h"
 #include "Tower.generated.h"
 
 class UTowerAction;
 
 UCLASS()
-class ATower : public APawn
+class ATower : public APawn, public ITeamAgentInterface
 {
 	GENERATED_BODY()
 
 public:
+
+	uint8 GetTeamId() override;
 
 	UPROPERTY(EditAnywhere, Instanced, Category = Actions)
 	TArray<UTowerAction*> Actions;
