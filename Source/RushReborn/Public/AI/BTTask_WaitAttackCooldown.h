448f8bef54b65ad139a061dd1dd3251b327d1e14
msmoiz <mustafa.moiz125@gmail.com>
2021-04-26 11:35:56 -0700

    Commented WaitAttackCooldown task.

--- Source/RushReborn/Public/AI/BTTask_WaitAttackCooldown.h
+++ Source/RushReborn/Public/AI/BTTask_WaitAttackCooldown.h
@@ -4,6 +4,11 @@
 #include "BehaviorTree/Tasks/BTTask_Wait.h"
 #include "BTTask_WaitAttackCooldown.generated.h"
 
+/**
+ * Wait for the duration of the owning
+ * actor's attack rate. Depends on the owning actor
+ * having a Stats component.
+ */
 UCLASS(HideCategories=(Wait))
 class UBTTask_WaitAttackCooldown : public UBTTask_Wait
 {
