e718416b43dca3a8ff874232ba3c9b8cc072386f
msmoiz <mustafa.moiz125@gmail.com>
2021-04-26 12:01:50 -0700

    Commented SearchForEnemy task.

--- Source/RushReborn/Public/AI/BTService_SearchForEnemy.h
+++ Source/RushReborn/Public/AI/BTService_SearchForEnemy.h
@@ -4,6 +4,12 @@
 #include "BehaviorTree/BTService.h"
 #include "BTService_SearchForEnemy.generated.h"
 
+/**
+ * Search for an enemy in the given radius,
+ * prioritizing:
+ * 1. Engagement status - Prefer unengaged enemies
+ * 2. Proximity - Prefer closer enemies
+ */
 UCLASS()
 class UBTService_SearchForEnemy : public UBTService
 {
@@ -19,9 +25,11 @@ protected:
 
 private:
 
+	/** The found enemy to target. */
 	UPROPERTY(EditAnywhere, Category = Blackboard)
 	FBlackboardKeySelector FoundEnemy;
-	
+
+	/** The distance from owner actor to search for enemies. */
 	UPROPERTY(EditAnywhere, Category = Search)
 	float SearchRadius;
 };
