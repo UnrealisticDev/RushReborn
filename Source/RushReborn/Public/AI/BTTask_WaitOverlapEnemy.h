3b82219af3625746532764f61fa918c97bb77226
msmoiz <mustafa.moiz125@gmail.com>
2021-04-26 11:34:05 -0700

    Commented WaitOverlapEnemy task.

--- Source/RushReborn/Public/AI/BTTask_WaitOverlapEnemy.h
+++ Source/RushReborn/Public/AI/BTTask_WaitOverlapEnemy.h
@@ -4,6 +4,13 @@
 #include "BehaviorTree/BTTaskNode.h"
 #include "BTTask_WaitOverlapEnemy.generated.h"
 
+/**
+ * Wait until the owner actor overlaps with an
+ * enemy. Naturally, requires the owner actor to
+ * have overlapping collision enabled.
+ *
+ * Task is instanced.
+ */
 UCLASS()
 class UBTTask_WaitOverlapEnemy : public UBTTaskNode
 {
@@ -15,6 +22,7 @@ public:
 
 	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 
+	/** Callback for when owner actor is overlapped. */
 	UFUNCTION()
 	void OnOwnerOverlapped(AActor* OwnerActor, AActor* OtherActor);
 
@@ -22,6 +30,10 @@ public:
 
 private:
 
+	/**
+	 * The owning behavior tree.
+	 * Cached to allow finishing task from latent callback.
+	 */
 	UPROPERTY()
 	TWeakObjectPtr<UBehaviorTreeComponent> OwnerComponent;
 };
