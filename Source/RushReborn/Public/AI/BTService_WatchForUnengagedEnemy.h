f460adf905401b8d7c7829da7c2f3036fac3af39
msmoiz <mustafa.moiz125@gmail.com>
2021-04-26 11:56:05 -0700

    Commented WatchForUnengagedEnemy task.

--- Source/RushReborn/Public/AI/BTService_WatchForUnengagedEnemy.h
+++ Source/RushReborn/Public/AI/BTService_WatchForUnengagedEnemy.h
@@ -4,6 +4,12 @@
 #include "BehaviorTree/BTService.h"
 #include "BTService_WatchForUnengagedEnemy.generated.h"
 
+/**
+ * Looks for unengaged enemies for the owner actor
+ * to engage. This prevents multiple defenders
+ * from tunnel-visioning onto a single enemy while
+ * other enemies run by.
+ */
 UCLASS()
 class UBTService_WatchForUnengagedEnemy : public UBTService
 {
@@ -16,12 +22,18 @@ public:
 
 protected:
 
+	/**
+	 * An unengaged enemy to target.
+	 * Will be set when found, but untouched otherwise.
+	 */
 	UPROPERTY(EditAnywhere, Category = Search)
 	FBlackboardKeySelector UnengagedEnemy;
-	
+
+	/** The distance from owner for searching for enemies. */
 	UPROPERTY(EditAnywhere, Category = Search)
 	float SearchRadius;
 
+	/** The defender AI state. */
 	UPROPERTY(EditAnywhere, Category = Search)
 	FBlackboardKeySelector State;
 };
