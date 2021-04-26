9f2a0c6dab55e9274e902c8f53858c38a8489e82
msmoiz <mustafa.moiz125@gmail.com>
2021-04-26 10:39:59 -0700

    Commented UReinforcementsAbility.

--- Source/RushReborn/Public/Abilities/ReinforcementsAbility.h
+++ Source/RushReborn/Public/Abilities/ReinforcementsAbility.h
@@ -4,6 +4,11 @@
 #include "Abilities/Ability.h"
 #include "ReinforcementsAbility.generated.h"
 
+/**
+ * The Reinforcements ability spawns
+ * two defenders of the specified class
+ * at the target location.
+ */
 UCLASS(Config=Game)
 class UReinforcementsAbility : public UAbility
 {
@@ -17,9 +22,14 @@ public:
 
 private:
 
+	/** The reinforcement class to spawn. */
 	UPROPERTY(Config)
 	FSoftClassPath SpawnClass;
 
+	/**
+	 * The offset from target location to spawn.
+	 * (So that spawns are staggered radially)
+	 */
 	UPROPERTY()
 	FVector SpawnOffset;
 };
